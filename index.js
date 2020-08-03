1eb98769806d2dd77c15d3441147d45ce20197f2
luo xing <925496743@qq.com>
2020-08-03 13:21:31 +0800

    first

---
+++ index.js
@@ -0,0 +1,93 @@
+let canvas = document.getElementById("canvas")
+let actions = document.getElementById("actions")
+let ctx = canvas.getContext("2d")
+autoSetCanvasSize(canvas)
+
+ctx.fillStyle = "black"
+ctx.strokeStyle = 'none'
+// ctx.lineWidth = 10
+// ctx.lineCap = 'round'
+
+listenToMouse(canvas)
+
+/* ** ** */
+function listenToMouse(canvas) {
+    let using = false
+    let lastPoint = {
+        x: undefined,
+        y: undefined
+    }
+
+    canvas.onmousedown = (e) => {
+        let x = e.clientX
+        let y = e.clientY
+        using = true
+        if (eraserEnabled) {
+            ctx.clearRect(x - 5, y - 5, 10, 10)
+        } else {
+            lastPoint = {
+                x,
+                y
+                }
+            }
+        }
+
+    canvas.onmousemove = (e) => {
+        let x = e.clientX
+        let y = e.clientY
+        if (!using) return
+        
+        if (eraserEnabled) {
+            ctx.clearRect(x - 5, y - 5, 10, 10)
+        } else {
+            drawLine(lastPoint.x, lastPoint.y, x, y)
+            lastPoint = {
+                x,
+                y
+            }
+        }
+    }
+
+    canvas.onmouseup = () => {
+        using = false
+    }
+}
+
+
+
+/****画线****/
+
+function drawLine (x1, y1, x2, y2) {
+    ctx.beginPath()
+    ctx.moveTo(x1, y1)
+    ctx.lineTo(x2, y2)
+    ctx.stroke()
+}
+
+/****橡皮擦****/
+
+let eraserEnabled = false
+
+eraser.onclick = () => {
+    eraserEnabled = true
+    actions.className = 'actions x'
+}
+brush.onclick = () => {
+    eraserEnabled = false
+    actions.className = 'actions'
+}
+
+/***Canvas***/
+
+function autoSetCanvasSize(canvas) {
+    setCanvasSize()
+    window.onresize = function() {
+        setCanvasSize()
+    }
+
+    function setCanvasSize() {
+        canvas.width =  document.documentElement.clientWidth
+        canvas.height =  document.documentElement.clientHeight
+    }
+}
+
